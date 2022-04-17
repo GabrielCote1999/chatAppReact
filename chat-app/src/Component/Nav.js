3f9998bfe13b11e44bae7751479c383135cdf838
Gabriel <gabrielcote1999@gmail.com>
2022-04-17 11:01:18 -0400

    <Route>: Added the routes to the navBar

--- chat-app/src/Component/Nav.js
+++ chat-app/src/Component/Nav.js
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
@@ -18,19 +18,27 @@ const Nav = () => {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-          >
-            
-          </IconButton>
+          ></IconButton>
+
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             MyChatApp
           </Typography>
-          <Button color="inherit">Register</Button>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Chat</Button>
+
           
+          <Button component={Link} to="/" color="inherit">
+            Home
+          </Button>
+          <Button component={Link} to="/signup" color="inherit">
+            Register
+          </Button>
+
+          <Button component = {Link} to = "/login" color="inherit">
+              Login
+              </Button>
+          <Button component ={Link} to ="/chat" color="inherit">Chat</Button>
         </Toolbar>
       </AppBar>
     </Box>
   );
-} 
+};
 export default Nav;
