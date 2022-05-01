4c4594442150c14d380268bd84a4d43df4f535ce
Gabriel <gabrielcote1999@gmail.com>
2022-05-01 17:55:00 -0400

    Changed the textBox

--- chat-app/src/Component/Chat.js
+++ chat-app/src/Component/Chat.js
@@ -2,16 +2,13 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import "./Chat.css";
 import SideBar from "./SideBar";
-import Messages from "./Messages"
-import MessageInput from './MessageInput';
+import Messages from "./Messages";
+import MessageInput from "./MessageInput";
 
 function Chat() {
-  
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
- 
-
     const newSocket = io(`http://localhost:3000`);
 
     setSocket(newSocket);
@@ -20,25 +17,22 @@ function Chat() {
     return () => newSocket.close();
   }, [setSocket]);
 
-  
   return (
     <div className="Chat">
-     
+      {/*
       <SideBar className="side" />
-  
+    */}
       <section className="chat">
         <h1>This is the chatRoom</h1>
 
-        { socket ? (
-        <div className="chat-container">
-          <Messages socket={socket} />
-          <MessageInput socket={socket} />
-          
-        </div>
-      ) : (
-        <div>Not Connected</div>
-      )}
-
+        {socket ? (
+          <div className="chat-container">
+            <Messages socket={socket} />
+            <MessageInput socket={socket} />
+          </div>
+        ) : (
+          <div>Not Connected</div>
+        )}
       </section>
     </div>
   );
--- chat-app/src/Component/MessageInput.js
+++ chat-app/src/Component/MessageInput.js
@@ -1,21 +1,39 @@
-import React, { useState } from 'react';
-import './MessageInput.css';
+import { TextField } from "@mui/material";
+import e from "cors";
+import React, { useEffect, useState } from "react";
+import "./MessageInput.css";
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
 
-const NewMessage = ({socket}) => {
-  const [value, setValue] = useState('');
+const NewMessage = ({ socket }) => {
+  const [error, setError] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [value, setValue] = useState("");
+  const [counter, setCounter] = useState(0);
+
+  
   const submitForm = (e) => {
-    e.preventDefault();
-    socket.emit('message', value);
     
-    setValue('');
+    if ((value = "")) {
+      
+      e.preventDefault();
+      socket.emit("message", value);
+      setValue("");
+    }
   };
 
   return (
     <form onSubmit={submitForm}>
-      <input
+
+      
+      <TextField
+        error={e.length < 1}
+        id="outlined-basic"
+        label={"Write something"}
+        variant="outlined"
         autoFocus
         value={value}
-        placeholder="Type your message"
+     
         onChange={(e) => {
           setValue(e.currentTarget.value);
         }}
