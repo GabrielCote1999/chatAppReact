84dd3f29a16138110d24f330ad3bdfc08c94ae0d
Gabriel <gabrielcote1999@gmail.com>
2022-05-03 13:24:12 -0400

    <Nav>: redirecting the routes for logout and login

--- chat-app/src/Component/Nav.js
+++ chat-app/src/Component/Nav.js
@@ -6,60 +6,57 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import { Link } from "react-router-dom";
-import "./Nav.css"
+import "./Nav.css";
 
 const Nav = (props) => {
-
   const token = props.token;
 
- 
   return (
     <div className="nav">
-    <Box 
-    sx={{ flexGrow: 1, zIndex:-2 }}
-    >
-      <AppBar position="static"
-      >
-        <Toolbar >
-          <IconButton
-          
-            size="large"
-            edge="start"
-            color="inherit"
-            
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          ></IconButton>
+      <Box sx={{ flexGrow: 1, zIndex: -2 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            ></IconButton>
 
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            MyChatApp
-          </Typography>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              MyChatApp
+            </Typography>
 
-          
-          <Button component={Link} to="/" color="inherit">
-            Home
-          </Button>
-          <Button component={Link} to="/signup" color="inherit">
-            Register
-          </Button>
+            <Button component={Link} to="/" color="inherit">
+              Home
+            </Button>
+            {!token ? (
+            <Button component={Link} to="/signup" color="inherit">
+              Register
+            </Button>
+            ): (
+              <></>
+            )}
 
-         {!token? <Button component = {Link} to = "/login" color="inherit">Login</Button>: <Button component = {Link} to = "/login" color="inherit">Logout</Button>}
+            {!token ? (
+              <Button component={Link} to="/" color="inherit">
+                Login
+              </Button>
+            ) : (
+              <Button component={Link} to="/" color="inherit">
+                Logout
+              </Button>
+            )}
 
-         
-       
-           
-          
-          
-             
-          <Button component ={Link} to ="/chat" color="inherit">Chat</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+            <Button component={Link} to="/chat" color="inherit">
+              Chat
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </div>
-  
   );
-  
-}
-
+};
 
 export default Nav;
